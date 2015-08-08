5f1512e4e97c86e0471a767b779b06fb23d22839
ahustjust <593283083@qq.com>
2015-08-08 17:38:36 +0800

    测试

---
+++ git项目测试/testa.h
@@ -0,0 +1,13 @@
+//
+//  testa.h
+//  git项目测试
+//
+//  Created by MAC-yanghua on 15/8/8.
+//  Copyright (c) 2015年 MAC-yanghua. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface testa : NSObject
+
+@end
---
+++ git项目测试/testa.m
@@ -0,0 +1,13 @@
+//
+//  testa.m
+//  git项目测试
+//
+//  Created by MAC-yanghua on 15/8/8.
+//  Copyright (c) 2015年 MAC-yanghua. All rights reserved.
+//
+
+#import "testa.h"
+
+@implementation testa
+
+@end
